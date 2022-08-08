96e0a4744021e001c9616a4e7a910c10fd9a8c69
Janet Enyan <jenyan002@st.ug.edu.gh>
2022-08-08 12:26:04 +0000

    Initial commit.---------------

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "calculating the sum of even numbers from 1 to 10000"<< endl;
+    int sum =0;
+    int countEven =0;
+
+    for (int a=2; a<= 10000;a++){
+       if (a%2 ==0){
+            sum = sum+ a;
+            countEven++;
+       }
+    }
+    cout<<"The number of even numbers available in the given range is "<< countEven<<endl<<endl;
+    cout<<"the sum of the even is:"<< sum << endl<<endl;
+    float mean;
+    mean = sum /countEven;
+    cout<< "The mean is therefore calculated as "<<mean<<endl;
+    return 0;
+}
